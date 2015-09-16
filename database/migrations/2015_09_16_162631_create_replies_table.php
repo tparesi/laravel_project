55d7d6836b9fee9cab39bafb0d33ed41ebe803e8
tparesi <tparesi@gmail.com>
2015-09-16 12:33:42 -0400

    Create db schema

---
+++ database/migrations/2015_09_16_162631_create_replies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->text('body');
+            $table->integer('user_id');
+            $table->integer('tweet_id');
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('replies');
+    }
+}
